55b235351b0219a3d6c2fd711edef468d3a3372f
RoGaTT <firestyl@yandex.ru>
2022-06-13 20:23:35 +0300

    update contacts

--- src/const/contacts.ts
+++ src/const/contacts.ts
@@ -1,7 +1,7 @@
 const CONTACTS = {
   email: 'info@coinsshop.su',
   telegram: '@@Coinsshop_su',
-  telegramLink: 'https://t.me/@Coinsshop_su',
+  telegramLink: 'https://t.me/Coinsshop_su',
   siteUrl: 'https://coinsshop.su',
   name: 'CoinsShop',
 };
