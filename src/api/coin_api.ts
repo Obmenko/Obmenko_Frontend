5965a7358051a28aff616c85c524fdab4a6f364c
RoGaTT <firestyl@yandex.ru>
2022-06-16 20:53:39 +0300

    update api

--- src/api/coin_api.ts
+++ src/api/coin_api.ts
@@ -2,13 +2,7 @@
 import axios from 'axios';
 import { CurrencyUnitEnum } from '@/types/exchange';
 
-const COIN_API_BASE = process.env.REACT_APP_COIN_API_BASE;
-const COIN_API_KEY = process.env.REACT_APP_COIN_API_KEY || '';
-
 type OExchangePair = {
-  time: string,
-  asset_id_base: string,
-  asset_id_quote: string,
   rate: number,
 }
 
@@ -17,8 +11,5 @@ export const getExchangePair = (
   from: CurrencyUnitEnum, to: CurrencyUnitEnum,
 ): Promise<OExchangePair> => axios({
   method: 'GET',
-  url: `${COIN_API_BASE}/exchangerate/${from}/${to}`,
-  headers: {
-    'X-CoinAPI-Key': COIN_API_KEY,
-  },
+  url: `${process.env.REACT_APP_API_BASE}/currency/${from}/${to}`,
 }).then((res) => res.data);
